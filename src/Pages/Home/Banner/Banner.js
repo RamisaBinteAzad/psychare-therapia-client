823a8a438670b5526cf7246b50e37a30a1bc016c
RamisaBinteAzad <tasnimlamia52@gmail.com>
2022-11-09 16:23:33 +0600

    Changes in Banner And Footer is Added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,30 +1,30 @@
 import { faArrowRight, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  } from "react-icons/fa";
+import {} from "react-icons/fa";
 import React from "react";
 import { Link } from "react-router-dom";
 import img1 from "../../../assets/images/Couple.jpg";
 import img2 from "../../../assets/images/Depression.jpg";
 import img3 from "../../../assets/images/Family.jpg";
-import './Banner.css'
+import "./Banner.css";
 
 const Banner = () => {
   return (
     <div
       id="carouselExampleControls"
-      className="carousel slide"
+      className="carousel slide height-change w-100  "
       data-bs-ride="carousel"
     >
-      <div className="carousel-inner   ">
-        <div className="carousel-item active  ">
+      <div className="carousel-inner    ">
+        <div className="carousel-item active    ">
           <div className="card   text-bg-dark border-0 ">
             <img
               src={img1}
               className="card-img img-fluid opacity  w-100 h-100"
               alt=""
             />
-            <div className="card-img-overlay d-flex  container align-items-center  ">
-              <div className=" ">
+            <div className="card-img-overlay   container align-items-center     d-none d-md-flex ">
+              <div className="header">
                 <h1 className="display-4   text-light">Better Advice for</h1>
 
                 <h3 className="display-5   text-light">
@@ -64,6 +64,51 @@ const Banner = () => {
                 </div>
               </div>
             </div>
+            <div className="card-img-overlay d-flex  container     d-md-none d-flex py-auto mx-auto ">
+              <div className="header ">
+                <h2 className="   ">Better Advice for</h2>
+
+                <h3 className="">
+                  <i>Healthy And Happier </i>
+                </h3>
+                <h2 className="">Relationships</h2>
+
+                <span>
+                  <small className="text-gray   ">
+                    Mental Health is a majorn concern nowadays,
+                    <br />
+                    which can end up affecting relations
+                    <br />& giving diseases.
+                  </small>
+                </span>
+                <div className="">
+                  <Link to="/">
+                    <button
+                      type="button"
+                      className=" rounded   btn btn-sm border-0 text-white btn-bg-pink   me-3  px-3 "
+                    >
+                      Get Started
+                      <FontAwesomeIcon
+                        className="ms-2"
+                        icon={faArrowRight}
+                      ></FontAwesomeIcon>
+                    </button>
+                  </Link>
+                  <Link to="/">
+                    <button
+                      //   type="button"
+                      className=" rounded  btn btn-sm bg-light  text-primary border-0   px-3 "
+                    >
+                      Book Session
+                      <FontAwesomeIcon
+                        className="ms-2"
+                        icon={faSignInAlt}
+                      ></FontAwesomeIcon>
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="carousel-item">
@@ -73,7 +118,7 @@ const Banner = () => {
               className="card-img img-fluid opacity-25  w-100 h-100"
               alt=""
             />
-            <div className="card-img-overlay d-flex container align-items-center  ">
+            <div className="card-img-overlay d-none d-md-flex container align-items-center  ">
               <div className="">
                 <h1 className="display-4   text-light">Transform your</h1>
 
@@ -113,6 +158,50 @@ const Banner = () => {
                 </div>
               </div>
             </div>
+            <div className="card-img-overlay d-flex  container  border    d-md-none d-flex py-auto mx-auto ">
+              <div className="header ">
+                <h2 className="   ">Transform your</h2>
+
+                <h3 className="">
+                  <i>Mindset & Achieve </i>
+                </h3>
+                <h2 className="">your Goals.</h2>
+
+                <span>
+                  <small className="text-gray   ">
+                    Amazing therapies and advice to help you achieve your
+                    <br />
+                    goals and taking important decisions of your life.
+                  </small>
+                </span>
+                <div className="mt-2">
+                  <Link to="/">
+                    <button
+                      type="button"
+                      className=" rounded   btn btn-sm border-0 text-white btn-bg-pink   me-3  px-3 "
+                    >
+                      Get Started
+                      <FontAwesomeIcon
+                        className="ms-2"
+                        icon={faArrowRight}
+                      ></FontAwesomeIcon>
+                    </button>
+                  </Link>
+                  <Link to="/">
+                    <button
+                      //   type="button"
+                      className=" rounded  btn btn-sm bg-light  text-primary border-0   px-3 "
+                    >
+                      Book Session
+                      <FontAwesomeIcon
+                        className="ms-2"
+                        icon={faSignInAlt}
+                      ></FontAwesomeIcon>
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="carousel-item">
@@ -123,7 +212,7 @@ const Banner = () => {
               alt=""
             />
 
-            <div className="card-img-overlay d-flex container align-items-center   ">
+            <div className="card-img-overlay d-flex container align-items-center  d-none d-md-flex   ">
               <div className="">
                 <h1 className="display-4   text-light">Help family members</h1>
                 <h3 className="display-5   text-light">
@@ -154,6 +243,50 @@ const Banner = () => {
                     <button
                       //   type="button"
                       className=" rounded  btn-bg-white border-0   px-3 py-2"
+                    >
+                      Book Session
+                      <FontAwesomeIcon
+                        className="ms-2"
+                        icon={faSignInAlt}
+                      ></FontAwesomeIcon>
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="card-img-overlay d-flex  container  border    d-md-none d-flex py-auto mx-auto ">
+              <div className="header ">
+                <h2 className="   ">Help family members</h2>
+
+                <h3 className="">
+                  <i>improve communication</i>
+                </h3>
+                <h2 className=""> and resolve conflicts.</h2>
+
+                <span>
+                  <small className="text-gray   ">
+                    Learn new ways to to deepen family connections and
+                    <br />
+                    overcome unhealthy patterns of relating to each other.
+                  </small>
+                </span>
+                <div className="mt-2">
+                  <Link to="/">
+                    <button
+                      type="button"
+                      className=" rounded   btn btn-sm border-0 text-white btn-bg-pink   me-3  px-3 "
+                    >
+                      Get Started
+                      <FontAwesomeIcon
+                        className="ms-2"
+                        icon={faArrowRight}
+                      ></FontAwesomeIcon>
+                    </button>
+                  </Link>
+                  <Link to="/">
+                    <button
+                      //   type="button"
+                      className=" rounded  btn btn-sm bg-light  text-primary border-0   px-3 "
                     >
                       Book Session
                       <FontAwesomeIcon
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,12 +1,180 @@
-import React from 'react';
- 
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaGoogle, FaTwitter, FaInstagram, FaLinkedin, FaGithub,FaHome,FaEnvelope,FaPhoneAlt,FaClock,FaPhoneSquareAlt} from "react-icons/fa";
+import logo from "../../../assets/icons/psychology.png";
+import './Footer.css'
 
 const Footer = () => {
-    return (
-      <footer className="footer p-32 bg-slate-900 text-gray-300">
-         <h2>Footer</h2>
+  return (
+    <div className="bg-light shadow-lg">
+      <footer className="text-center text-lg-start  text-muted">
+        <section className="d-flex justify-content-center justify-content-lg-between p-4  ">
+          {/* <div className="me-5 d-none d-lg-block  text-white me-auto">
+            <span className="text-center">
+              Get connected with us on social networks:
+            </span>
+          </div> */}
+        </section>
+
+        <section className="">
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4">
+                <h6 className="text-uppercase text-white fw-bold mb-4">
+                  <div className="d-flex align-items-center  ">
+                    <img
+                      src={logo}
+                      alt="Logo"
+                      className="me-2  img-fluid  d-inline-block "
+                    />
+                    <div className=" fw-bold     align-items-center    ">
+                      <span className="fs-3  mt-2 fw-bold pink">Psychare</span>
+                      <p className=" fs-4 dark-blue my-0">Therapia</p>
+                    </div>
+                  </div>
+                </h6>
+                <p className="text-dark fw-bold">
+                  Being a full-time psychologist, I make my patients feel
+                  special so they can discuss their problems openly. My goal is
+                  to help people fighting their fears and life issues.
+                </p>
+                <div className="d-none d-lg-block ">
+                  <Link
+                    to=""
+                    className="
+                     me-4 text-reset"
+                  >
+                    <FaFacebook className="faFacebook"></FaFacebook>
+                  </Link>
+                  <Link to="" className="me-4 text-reset">
+                    <FaTwitter className="fa-twitter"></FaTwitter>
+                  </Link>
+                  <Link to="" className="me-4 text-reset">
+                    <FaGoogle className="fa-google"></FaGoogle>
+                  </Link>
+                  <Link to="" className="me-4 text-reset">
+                    <FaInstagram className="fa-instragram"></FaInstagram>
+                  </Link>
+
+                  <Link to="" className="me-4 text-reset">
+                    <FaLinkedin className="fa-Linkedin"></FaLinkedin>
+                  </Link>
+                  <Link to="" className="me-4 text-reset">
+                    <FaGithub className="fa-Github"></FaGithub>
+                  </Link>
+                </div>
+              </div>
+
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4 dark-blue">
+                  Services
+                  <hr />
+                </h6>
+                <p>
+                  <Link to="#!" className="text-dark  fw-bold">
+                    Individual Counseling
+                  </Link>
+                </p>
+                <p>
+                  <Link to="#!" className="text-dark  fw-bold">
+                    Marriage Counseling
+                  </Link>
+                </p>
+                <p>
+                  <Link to="#!" className="text-dark  fw-bold">
+                    Family Therapy
+                  </Link>
+                </p>
+                <p>
+                  <Link to="#!" className="text-dark  fw-bold">
+                    Child & Teen Therapy
+                  </Link>
+                </p>
+              </div>
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 className="text-uppercase  fw-bold mb-4 dark-blue">
+                  Contact Details
+                  <hr />
+                </h6>
+                <p className="text-dark fw-bold">
+                  <FaHome className="faFacebook me-3"></FaHome>
+                  Hotel Suite Palace, House# 31, Road# 13, Block# K Baridhara,
+                  Dhaka 1212
+                </p>
+                <p className="text-dark fw-bold">
+                  <FaEnvelope className="faFacebook me-3"></FaEnvelope>
+                  info@example.com
+                </p>
+                <p className="text-dark fw-bold">
+                  <FaPhoneAlt className="faFacebook me-3"></FaPhoneAlt>
+                  +01777-772763
+                </p>
+
+                <p className="text-dark fw-bold">
+                  <FaClock className="faFacebook me-3"></FaClock>
+                  Mon - Fri: ( 9am - 6pm ) Sat & Sun: CLOSED
+                </p>
+                <div className="d-lg-none mb-3 ">
+                  <Link
+                    to=""
+                    className="
+                     me-4 text-reset"
+                  >
+                    <FaFacebook className="faFacebook"></FaFacebook>
+                  </Link>
+                  <Link to="" className="me-4 text-reset">
+                    <FaTwitter className="fa-twitter"></FaTwitter>
+                  </Link>
+                  <Link to="" className="me-4 text-reset">
+                    <FaGoogle className="fa-google"></FaGoogle>
+                  </Link>
+                  <Link to="" className="me-4 text-reset">
+                    <FaInstagram className="fa-instragram"></FaInstagram>
+                  </Link>
+
+                  <Link to="" className="me-4 text-reset">
+                    <FaLinkedin className="fa-Linkedin"></FaLinkedin>
+                  </Link>
+                  <Link to="" className="me-4 text-reset">
+                    <FaGithub className="fa-Github"></FaGithub>
+                  </Link>
+                </div>
+              </div>
+
+              <div className="col-md-3 col-lg-2 col-xl-2   mb-4">
+                <h6 className="text-uppercase fw-bold mb-4 dark-blue">
+                  Book Appointment
+                  <hr />
+                </h6>
+                <div className="border grayish-bg-pink w-lg-100  py-3 px-2 mb-2 rounded shadow">
+                  <p className="text-uppercase faFacebook fw-bold">
+                    Let's Chat{" "}
+                  </p>
+
+                  <FaPhoneSquareAlt className="  faFacebook"></FaPhoneSquareAlt>
+                  <small className="text-muted fw-bold   ">+01777-772763</small>
+                </div>
+                <small className="  fw-bold text-dark  ">
+                  Contact us now for
+                  <br />
+                  a quote about consultation <br />
+                  <span className="faFacebook">( Available 24/7 )</span>
+                </small>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div className="text-center p-4 grayish-pink shadow-lg">
+          <span className="text-dark fw-bold"> © 2021 Copyright:</span>
+          <Link className="text-reset  dark-blue fw-bold" to="/">
+            <span className="me-2  mt-2 fw-bold pink">Psychare</span>
+            <span className="  dark-blue my-0">Therapia</span>
+          </Link>
+        </div>
       </footer>
-    );
+    </div>
+  );
 };
 
 export default Footer;
